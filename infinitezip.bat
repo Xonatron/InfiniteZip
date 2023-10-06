117ed60cf21b571c9d4f9a5e96779fdd29f50e7f
Xonatron <matt@xona.com>
2023-10-05 23:47:44 -0300

    added temp folder to hide temp files

--- infinitezip.bat
+++ infinitezip.bat
@@ -4,43 +4,45 @@ setlocal enabledelayedexpansion
 rem Define the path to WinRAR executable (adjust this path if needed)
 set winrarPath="C:\Program Files\WinRAR\WinRAR.exe"
 
-rem Define output folder
-set outputFolder="output"
+rem Define folders (temp folder will be inside output folder)
+set outputFolder=output
+set tempFolder=temp
 
-rem Create output folder
+rem Create folders
 mkdir "%outputFolder%"
+mkdir "%outputFolder%\%tempFolder%"
 
 rem Change the current working folder to output folder
 cd /d "%outputFolder%"
 
 rem Create an initial ZIP file containing the TXT file
-%winrarPath% a -ep1 -o -y -inul -IBCK -m5 infinite.zip "..\infinitezip.bat"
+%winrarPath% a -ep1 -o -y -inul -IBCK -m5 "%tempFolder%\infinite.zip" "..\infinitezip.bat"
 
-rem Initialize the count (start at 1 matches how many .zips there are in the .zip)
+rem Initialize count (count should match number of .zip files, including the containing .zip file itself)
 set count=1
 
 :loop
 
+rem Increment the count
+set /a count+=1
+
 rem Create a new ZIP file containing the previous ZIP file
-%winrarPath% a -ep1 -o -y -inul -IBCK -m5 temp.zip infinite.zip
+%winrarPath% a -ep1 -o -y -inul -IBCK -m5 "%tempFolder%\temp.zip" "%tempFolder%\infinite.zip"
 
 rem Delete old ZIP file
-del infinite.zip
+del "%tempFolder%\infinite.zip"
 
 rem Rename new ZIP file from temporary name to proper name
-rename temp.zip infinite.zip
+rename "%tempFolder%\temp.zip" "infinite.zip"
 
 rem Create a folder with the current count
 mkdir "!count!"
 
 rem Copy the current ZIP file into the folder
-copy infinite.zip "!count!" > nul
+copy "%tempFolder%\infinite.zip" "!count!" > nul
 
 rem Display a message indicating each iteration
 echo infinite.zip file updated and copied to folder !count!.
-
-rem Increment the count
-set /a count+=1
 
 goto loop
 
